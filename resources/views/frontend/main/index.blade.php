6098b2ed4ace7b8675c6fb641dad2887332c67b1
ArminP <ArminPouralvani@outlook.com>
2023-08-05 13:19:46 +0330

    configured the main frontend view to show only active posts

--- resources/views/frontend/main/index.blade.php
+++ resources/views/frontend/main/index.blade.php
@@ -5,6 +5,7 @@
     <div class="col-xl-8 py-5 px-md-5">
         <div class="row pt-md-4">
             @foreach($posts as $post)
+                @if($post->status == 1)
             <div class="col-md-12">
                 <div class="blog-entry ftco-animate d-md-flex">
                     <a href="{{ route('frontend.posts.show' , $post->id) }}" class="img img-2" style="background-image: url({{ $post->photo ? $post->photo->path : "https://www.placehold.it/900x300" }});"></a>
@@ -17,11 +18,12 @@
                                 <span><i class="icon-comment2 mr-2"></i>5 Comment</span>
                             </p>
                         </div>
-                        <p class="mb-4">{{ Str::limit($post->description , 45) }}.</p>
-                        <p><a href="#" class="btn-custom">Read More <span class="ion-ios-arrow-forward"></span></a></p>
+                        <p class="mb-4">{{ Str::limit($post->description , 45) }}</p>
+                        <p><a href="{{ route('frontend.posts.show' , $post->id) }}" class="btn-custom">Read More <span class="ion-ios-arrow-forward"></span></a></p>
                     </div>
                 </div>
             </div>
+                @endif
             @endforeach
         </div>
         <div class="row">
